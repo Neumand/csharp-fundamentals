fe41dfd6443a0a43727556a6d076ec56a88a385f
David Neuman <david.m.neuman@gmail.com>
2019-06-09 11:00:02 -0400

    Initial commit

---
+++ Notes.md
@@ -0,0 +1,13 @@
+# C# Fundamentals -Pluralsight
+
+### Running and Buildng a .NET application
+
+* dotnet restore checks to see if there are any dependencies
+* dotnet build then sees the full picture of the application
+* C# compiler transforms the source code into binary representation necessary to run the application
+* NuGet: package manager for .NET applications (similar to npm for Node)
+* DLL file: assembly, output of the compiler that contains the code in binary format
+* dotnet is required to run compiled code
+  Eg. `dotnet bin\debug\netcoreapp2.1\Gradebook.dll`
+
+### Type Parameters
---
+++ src/Gradebook/Program.cs
@@ -0,0 +1,32 @@
+﻿// Tells the C# compiler that it should be looking in these namespaces
+using System;
+using System.Collections.Generic;
+
+namespace Gradebook
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+      grades.Add(56.1);
+      var result = 0.0;
+      foreach (var grade in grades)
+      {
+        result += grade;
+      }
+      var average = result / grades.Count;
+      Console.WriteLine($"The sum of all grades is {result:N2}");
+      Console.WriteLine($"The average grade is {average:N2}");
+
+      if (args.Length > 0)
+      {
+        Console.WriteLine($"Hello, {args[0]}!");
+      }
+      else
+      {
+        Console.WriteLine("Hello!");
+      }
+    }
+  }
+}
