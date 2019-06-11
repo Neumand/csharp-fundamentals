c7723b54d97bbeb6b44882dbe15067187e3530d5
David Neuman <david.m.neuman@gmail.com>
2019-06-10 20:15:34 -0400

    Encapsulated logic for the gradebook in its own class

--- src/Gradebook/Book.cs
+++ src/Gradebook/Book.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace Gradebook
@@ -11,6 +12,28 @@ namespace Gradebook
     public void AddGrade(double grade)
     {
       grades.Add(grade);
+    }
+
+    public void ShowStatistics()
+    {
+      var result = 0.0;
+      var highGrade = double.MinValue;
+      var lowGrade = double.MaxValue;
+
+      foreach (var grade in grades)
+      {
+        lowGrade = Math.Min(grade, lowGrade);
+        highGrade = Math.Max(grade, highGrade);
+        result += grade;
+      }
+
+      var average = result / grades.Count;
+
+      // Console Outputs
+      Console.WriteLine($"The lowest grade is {lowGrade}");
+      Console.WriteLine($"The highest grade is {highGrade}");
+      Console.WriteLine($"The sum of all grades is {result:N2}");
+      Console.WriteLine($"The average grade is {average:N2}");
     }
 
     List<double> grades;
--- src/Gradebook/Program.cs
+++ src/Gradebook/Program.cs
@@ -9,18 +9,11 @@ namespace Gradebook
   {
     static void Main(string[] args)
     {
-      var book = new Book();
+      var book = new Book("David's Grade Book");
       book.AddGrade(89.1);
-      var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
-      grades.Add(56.1);
-      var result = 0.0;
-      foreach (var grade in grades)
-      {
-        result += grade;
-      }
-      var average = result / grades.Count;
-      Console.WriteLine($"The sum of all grades is {result:N2}");
-      Console.WriteLine($"The average grade is {average:N2}");
+      book.AddGrade(90.5);
+      book.AddGrade(77.5);
+      book.ShowStatistics();
     }
   }
 }
